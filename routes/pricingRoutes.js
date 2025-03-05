8aeb99192e1212d7224f95eca052fe8b2aca249c
MalekAhmed01 <136865835+MalekAhmed01@users.noreply.github.com>
2025-03-05 09:00:35 +0200

    Update pricingRoutes.js

--- routes/pricingRoutes.js
+++ routes/pricingRoutes.js
@@ -1,4 +1,3 @@
-// routes/pricing.js
 const express = require("express");
 const pricingController = require("../controllers/pricingController");
 const { authenticate, authorizeAdmin } = require("../middlewares/authMiddleware");
@@ -8,13 +7,10 @@ const router = express.Router();
 router.use(authenticate);
 router.use(authorizeAdmin);
 
-// GET /api/pricing -> Retrieve all pricing records
 router.get("/", pricingController.getAllPricing);
 
-// POST /api/pricing -> Create a new pricing record (will fail if there are already 2)
 router.post("/", pricingController.createPricing);
 
-// PUT /api/pricing/:id -> Update pricing details (plan name and price)
 router.put("/:id", pricingController.updatePricing);
 
 module.exports = router;
