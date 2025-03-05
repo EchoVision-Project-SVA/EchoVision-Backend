86ec12837070cfdfad4c01bc5ca0f3bb3bce0592
MalekAhmed01 <136865835+MalekAhmed01@users.noreply.github.com>
2025-03-05 09:00:55 +0200

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/videoController');
 
-// POST endpoint to upload a video
 router.post('/', videoController.uploadVideo);
 
-// PUT endpoint to update the video status
 router.put('/:id', videoController.updateVideoStatus);
 
-// GET endpoints to retrieve videos
 router.get('/', videoController.getAllVideos);
 router.get('/:id', videoController.getVideoById);
 
